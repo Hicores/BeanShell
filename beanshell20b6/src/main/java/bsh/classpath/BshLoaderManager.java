674f7c5dd311eb8cd2afca09698c3cbc6a4b5739
Hicores <me@hicore.cc>
2022-07-23 14:24:48 +0800

    test

--- beanshell20b6/src/main/java/bsh/classpath/BshLoaderManager.java
+++ beanshell20b6/src/main/java/bsh/classpath/BshLoaderManager.java
@@ -10,11 +10,13 @@ public class BshLoaderManager {
         loaders.add(loader);
     }
     public static Class<?> findClass(String className) throws ClassNotFoundException {
+
         for (ClassLoader loader : loaders){
             try{
                 return loader.loadClass(className);
             }catch (ClassNotFoundException ignored){ }
         }
+        System.out.println("BshClassLoader, can't load class "+className);
         throw new ClassNotFoundException("Can't load class " + className + " in extra classloaders.");
     }
 }
